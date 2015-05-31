7e5a23cfe50a987504a3526d08a092746b0f5650
Candypz <candy_pz@outlook.com>
2015-05-31 23:11:22 +0800

    SearchTree

---
+++ BinaryTree/BTmain.cpp
@@ -0,0 +1,44 @@
+#include "BinaryTree.h"
+
+int BTmain() {
+    BinaryTree<char> tree;
+    TreeNode<char> add, sub, mul, div, a, b, c, d, e;
+
+    add.data = '+';
+    sub.data = '-';
+    mul.data = '*';
+    div.data = '/';
+    a.data = 'A';
+    b.data = 'B';
+    c.data = 'C';
+    d.data = 'D';
+    e.data = 'E';
+
+    tree.root = &add;
+    add.leftChild = &sub;
+    add.rightChild = &e;
+    sub.leftChild = &mul;
+    sub.rightChild = &d;
+    mul.leftChild = &div;
+    mul.rightChild = &c;
+    div.leftChild = &a;
+    div.rightChild = &b;
+
+    
+    tree.inOrder();
+    std::cout << "中序遍历" << std::endl;
+
+    
+    tree.preOrder();
+    std::cout << "前序遍历" << std::endl;
+
+
+    tree.postOrder();
+    std::cout << "后续遍历" << std::endl;
+
+
+    tree.levelOrder();
+    std::cout << "层序遍历" << std::endl;
+
+    return 0;
+}
--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -1,44 +1,30 @@
-#include "BinaryTree.h"
+#include <iostream>
+#include "BinarySearchTree.h"
 
 int main() {
-    BinaryTree<char> tree;
-    TreeNode<char> add, sub, mul, div, a, b, c, d, e;
+    BinarySerchTree<int> m;
+    Element<int> a, b, c, d, e, f, g, h, i, j;
+    a.key = 5;
+    b.key = 3;
+    c.key = 12;
+    d.key = 3;
+    e.key = 15;
+    f.key = 2;
+    g.key = 8;
+    h.key = 22;
+    i.key = 20;
+    j.key = 9;
 
-    add.data = '+';
-    sub.data = '-';
-    mul.data = '*';
-    div.data = '/';
-    a.data = 'A';
-    b.data = 'B';
-    c.data = 'C';
-    d.data = 'D';
-    e.data = 'E';
-
-    tree.root = &add;
-    add.leftChild = &sub;
-    add.rightChild = &e;
-    sub.leftChild = &mul;
-    sub.rightChild = &d;
-    mul.leftChild = &div;
-    mul.rightChild = &c;
-    div.leftChild = &a;
-    div.rightChild = &b;
-
-    
-    tree.inOrder();
-    std::cout << "中序遍历" << std::endl;
-
-    
-    tree.preOrder();
-    std::cout << "前序遍历" << std::endl;
-
-
-    tree.postOrder();
-    std::cout << "后续遍历" << std::endl;
-
-
-    tree.levelOrder();
-    std::cout << "层序遍历" << std::endl;
-
+    m.insert(a);
+    m.insert(b);
+    m.insert(c);
+    m.insert(f);
+    m.display();
+    BstNode<int>*ptr;
+    ptr = m.search(f);
+    std::cout << ptr->data.key;
+    BstNode<int>*ptr2;
+    ptr2 = m.search(f);
+    std::cout << ptr2->data.key;
     return 0;
 }
